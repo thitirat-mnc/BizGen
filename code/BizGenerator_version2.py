560c43ec4bd18ec9c26fa9fc2679d51fb15ec3b4
thitirat-mnc <134206687+thitirat-mnc@users.noreply.github.com>
2023-09-16 23:47:14 +0700

    Update BizGenerator_version2.py

--- code/BizGenerator_version2.py
+++ code/BizGenerator_version2.py
@@ -17,8 +17,8 @@ st.set_page_config(
 with st.sidebar:
     st.markdown("# Sparks New Business Ideas👩‍💼📈")
     st.markdown("""
-        Leverage the OpenAI API, along with the Langchain Python library, to generate a list of business name, 
-        competitive advantages and implementation plan
+        Leverage the OpenAI API, along with the Langchain Python library, to generate a list of business names, 
+        competitive advantages and specific implementation plan
         based on the context given.
         """)
     st.markdown("---")
