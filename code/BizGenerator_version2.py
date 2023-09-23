ed3cfd077a9eb1d6278cb9093908e44e8729fdca
thitirat-mnc <134206687+thitirat-mnc@users.noreply.github.com>
2023-09-23 21:32:53 +0700

    Update BizGenerator.py

--- code/BizGenerator_version2.py
+++ code/BizGenerator_version2.py
@@ -73,13 +73,13 @@ else:
 
                 Context: {text}
                 
-                Business Name:
+                **Business Name:**
                 
-                Vision:
+                **Vision:**
                 
-                Mission:
+                **Mission:**
                 
-                Competitive Advantage:
+                **Competitive Advantage:**
                 """
 
             business_prompt_template = PromptTemplate(
@@ -103,6 +103,50 @@ else:
             st.subheader(f"🔗 Business {i + 1}")
             st.write(business)
 
+            STP_template = f"""
+                As an experienced business consultant, your task is to create a concise STP (Segmentation, Targeting, Positioning) analysis for Business Idea {i + 1}. \
+                Your response, limited to 100 words, should be in the same language as the context.
+
+                Business Idea {i + 1}:
+                {business}
+
+                
+                Please structure your response as follows:
+
+                **Segmentation Strategy:**
+
+                **Targeting Strategy:**
+
+                **Positioning Strategy:**
+
+                Imagine you are guiding the business with a clear, actionable strategy as a business consultant:
+
+                """
+            
+            STP_prompt_template = PromptTemplate(
+                input_variables=[],
+                template=STP_template,
+                output_variables=["STP"]
+            )
+            
+            # Create the Langchain chain for STP
+            STP_chain = LLMChain(llm=llm, prompt=STP_prompt_template, output_key="STP")
+
+            second_chain = SequentialChain(
+                chains=[business_chain, STP_chain],
+                input_variables=[],
+                output_variables=[
+                    "business_info",
+                    "STP"
+                ],
+                verbose=True
+            )
+            
+            STP_output = second_chain({})
+            with st.expander(f"📮 STP "):    
+                st.write(STP_output["STP"])
+            
+
             plan_template = f"""
                 As an experienced business consultant, your task is to create a concise implementation plan for Business Idea {i + 1}. \
                 Your response, limited to 100 words, should be in the same language as the context.
@@ -122,11 +166,11 @@ else:
             )
 
             # Create the Langchain chain for generating plan
-            consultant_chain = LLMChain(llm=llm, prompt=plan_prompt_template, output_key="plan")
+            plan_chain = LLMChain(llm=llm, prompt=plan_prompt_template, output_key="plan")
 
-            # This is the overall chain where we run business generation and plan generation in sequence
-            overall_chain = SequentialChain(
-                chains=[business_chain, consultant_chain],
+            # This is the second chain where we run business generation and plan generation in sequence
+            third_chain = SequentialChain(
+                chains=[business_chain, plan_chain],
                 input_variables=[],
                 output_variables=[
                     "business_info",
@@ -135,10 +179,66 @@ else:
                 verbose=True
             )
 
-            # Generate plan using consultant_chain
-            plan_output = overall_chain({})
-            with st.expander(f"📃 Plan for Business {i + 1}"):    
+            # Generate plan
+            plan_output = third_chain({})
+            with st.expander(f"📓 Plan for Business {i + 1}"):    
                 st.write(plan_output["plan"])
+
+            criticism_template = f"""
+            Imagine you are a business critic tasked with evaluating Business Idea {i + 1}, its STP Analysis, and its Implementation Plan. 
+            Your response, limited to 100 words, should be in the same language as the context.
+
+            Business Idea {i + 1}:
+            {business}
+
+            STP Analysis:
+            {STP_output["STP"]}
+
+            Implementation Plan:
+            {plan_output["plan"]}
+            
+            Provide the answer in the following structure:
+
+            **Business Idea Feedback:**
+            1. *Strengths:* [Highlight the strengths of the business idea.]
+            2. *Suggestions for Improvement:* [Offer constructive suggestions for enhancing the business idea.]
+
+            **STP Analysis Feedback:**
+            1. *Strengths:* [Identify the strengths of the STP analysis.]
+            2. *Suggestions for Improvement:* [Propose improvements to the STP analysis.]
+
+            **Implementation Plan Feedback:**
+            1. *Strengths:* [Point out the strengths of the implementation plan.]
+            2. *Suggestions for Improvement:* [Provide recommendations for improving the implementation plan.]
+
+            Your input, structured in numbered lists, will contribute to the refinement and enhancement of these critical components of the business proposal.
+            """
+
+
+            criticism_prompt_template = PromptTemplate(
+                input_variables=[],
+                template=criticism_template,
+                output_variables=["criticism"]
+            )
+
+            # Create the Langchain chain for generating criticism
+            criticism_chain = LLMChain(llm=llm, prompt=criticism_prompt_template, output_key="criticism")
+            
+            fourth_chain = SequentialChain(
+                chains=[business_chain, STP_chain, plan_chain, criticism_chain],
+                input_variables=[],
+                output_variables=[
+                    "business_info",
+                    "criticism"
+                ],
+                verbose=True
+            )
+
+            # Generate plan
+            criticism_output = fourth_chain({})
+            with st.expander(f"🔖 Criticism for Business {i + 1}"):    
+                st.write(criticism_output["criticism"])
+
 
         # Remove the "Generating ideas..." message once results are ready
         if generating_message:
@@ -146,6 +246,6 @@ else:
 
         st.markdown("---")
         st.markdown("Feel free to explore more business ideas and refine your concepts!")
-        
+
 
     
